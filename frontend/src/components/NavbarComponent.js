e404b659ce8323e828b14cdc370cdae402934b11
Sarwar <sarwar@edunetfoudnation.org>
2025-03-31 19:18:16 +0530

    added respon

--- frontend/src/components/NavbarComponent.js
+++ frontend/src/components/NavbarComponent.js
@@ -5,14 +5,27 @@ function NavbarComponent({ user }) {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
+        {/* Logo */}
         <Link className="navbar-brand" to="/">MyApp</Link>
-        <div className="collapse navbar-collapse justify-content-end">
+
+        {/* Mobile Toggle Button */}
+        <button 
+          className="navbar-toggler" 
+          type="button" 
+          data-bs-toggle="collapse" 
+          data-bs-target="#navbarNav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Navbar Links */}
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
           <ul className="navbar-nav">
             {user ? (
               <>
                 <li className="nav-item">
-                  <Link className="nav-link" to="/profile">
-                    {user.username.toUpperCase()} {/* ✅ Username in uppercase */}
+                  <Link className="nav-link text-uppercase" to="/profile">
+                    {user.username}
                   </Link>
                 </li>
                 <li className="nav-item">
