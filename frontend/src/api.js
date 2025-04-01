3da768accb7056f5b47b4c341a2a6509daed64aa
Sarwar <sarwar@edunetfoudnation.org>
2025-04-01 11:40:36 +0530

    server

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const API_URL = "http://localhost:5000/api";
-const API_URL = "https://project-sigma-ochre.vercel.app/api";
+const API_URL = "http://localhost:5000/api";
+// const API_URL = "https://project-sigma-ochre.vercel.app/api";
 
 export const registerUser = async (userData) => {
   return await axios.post(`${API_URL}/auth/register`, userData);
