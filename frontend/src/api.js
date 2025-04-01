eadf9c6717a9db509fd84ca76d7fc6970057f756
Sarwar <sarwar@edunetfoudnation.org>
2025-04-01 11:28:54 +0530

    4 modified

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -5,21 +5,21 @@ const API_URL = "https://project-sigma-ochre.vercel.app/api";
 
 
 export const registerUser = async (userData) => {
-  return await axios.post(`${API_URL}/auth/register`, userData);
+  return await axios.post(`${API_URL}/api/auth/register`, userData);
 };
 
 export const loginUser = async (userData) => {
-  return await axios.post(`${API_URL}/auth/login`, userData);
+  return await axios.post(`${API_URL}/api/auth/login`, userData);
 };
 
 export const getProfile = async (token) => {
-  return await axios.get(`${API_URL}/user/profile`, {
+  return await axios.get(`${API_URL}/api/user/profile`, {
     headers: { Authorization: token },
   });
 };
 
 export const updateProfile = async (token, formData) => {
-  return await axios.put(`${API_URL}/user/profile`, formData, {
+  return await axios.put(`${API_URL}/api/user/profile`, formData, {
     headers: { Authorization: token, "Content-Type": "multipart/form-data" },
   });
 };
